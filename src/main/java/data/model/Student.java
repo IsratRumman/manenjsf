05d133379527e34630af4f1f4e8365d251fa258c
BUP <isratrumman25@gmail.com>
2018-04-23 02:17:03 +0600

    i am pangkha

---
+++ src/main/java/data/model/Student.java
@@ -0,0 +1,34 @@
+package data.model;
+
+public class Student {
+    private String studentFirstName;
+    private String studentLastName;
+    private Integer studentAge;
+
+    public Student() {
+    }
+
+    public String getStudentFirstName() {
+        return studentFirstName;
+    }
+
+    public void setStudentFirstName(String studentFirstName) {
+        this.studentFirstName = studentFirstName;
+    }
+
+    public String getStudentLastName() {
+        return studentLastName;
+    }
+
+    public void setStudentLastName(String studentLastName) {
+        this.studentLastName = studentLastName;
+    }
+
+    public Integer getStudentAge() {
+        return studentAge;
+    }
+
+    public void setStudentAge(Integer studentAge) {
+        this.studentAge = studentAge;
+    }
+}
